8eae941756b0dcaa2920c6c762aac734caa98697
dekr1sh <krishsonigra2175@gmail.com>
2024-08-21 22:19:41 +0530

    Setup the project

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./src/main.js",
+    output: {
+        filename: "script.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+}
