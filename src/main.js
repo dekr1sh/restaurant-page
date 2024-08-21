14bf29bf681ff4e167a9a4164e7d4020be64dbb0
dekr1sh <krishsonigra2175@gmail.com>
2024-08-22 01:22:01 +0530

    Write the tab-switching logic

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-import './style.css';
+import './style.css'; 
+import loadHome from './modules/home';
+import loadMenu from './modules/menu';
+import loadContact from './modules/contact';
+
+loadHome();
+
+document.querySelector('#home-tab').addEventListener('click', loadHome);
+document.querySelector('#menu-tab').addEventListener('click', loadMenu);
+document.querySelector('#contact-tab').addEventListener('click', loadContact);
